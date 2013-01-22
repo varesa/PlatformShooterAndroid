5431018e8c4832c333f4104353cc204ab552c837
Esa Varemo <esa@kuivanto.fi>
2013-01-22 15:39:21 +0200

    Porting the engine vol. 2

---
+++ src/fi/dy/esav/GameEngineAndroid/Rectangle.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package fi.dy.esav.GameEngineAndroid;
+
+public class Rectangle {
+
+	private double x, y, width, height;
+
+	public Rectangle() {
+	}
+
+	public Rectangle(double x, double y, double width, double height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public boolean intersects(Rectangle rect2) {
+		return this.x < rect2.x+rect2.width && this.x+this.width > rect2.x &&
+			    this.y < rect2.y+rect2.height && this.y+this.height > rect2.y;
+	}
+}
