83fe54938ce8cc66623a4caf5d7b1f9953affea8
Esa Varemo <esa@kuivanto.fi>
2013-01-22 14:25:07 +0200

    Deleted the layout

--- src/fi/dy/esav/PlatformShooter/PlatformShooterMainActivity.java
+++ src/fi/dy/esav/PlatformShooter/PlatformShooterMainActivity.java
@@ -9,12 +9,10 @@ public class PlatformShooterMainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.asd);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.asd, menu);
         return true;
     }
 }
